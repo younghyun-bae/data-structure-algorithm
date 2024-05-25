690dc0fd722ed1cd7de91f8b7c5d04958f0c37a9
Younghyun <yhbae97@gmail.com>
2024-05-25 21:45:54 +0200

    add problem solving patterns - frequency counter

--- src/3-problem-solving-approach.js
+++ src/3-problem-solving-approach.js
@@ -107,3 +107,116 @@ const charCount3 = (str) => {
 }
 console.log(charCount3("Hello, I am Younghyun >3<!"));
 
+
+// Problem Solving Patterns
+// Frequency Counter
+// Multiple Pointers
+// Sliding Window
+// Divide and Conquer
+// Dynamic Programming
+// Greedy Algorithms
+// Backtracking
+
+// Frequenct Counters
+// this pattern uses objects or sts to collect values/frequencies of values
+// can often avoid the need for nested loops or O(n^2) operations with arrays / strings
+
+// Write a function called same, which accepts two arrays. the function should return true if every value in the array has it's corresponding value squared in teh second array. the frequency of values must be the same.
+
+// A naive solution
+// Time Complexity - n^2
+const same1 = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2)
+    if (correctIndex === -1) {
+      return false;
+    }
+    arr2.splice(correctIndex, 1)
+  }
+  return true;
+}
+
+same1([1, 2, 3], [4, 1, 9]) //true
+same1([1, 2, 3], [1, 9]) // false
+same1([1, 2, 1], [4, 4, 1]) // false (must be same frequency)
+
+//Refactored
+// worse
+const same2 = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {}
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+  }
+  for (let key in frequencyCounter1) {
+    if(!(key ** 2 in frequencyCounter2)) {
+      return false
+    }
+    if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false
+    }
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  return true
+}
+
+console.log(same2([1, 2, 3, 2], [9, 1, 4, 4]));
+console.log(same2([1, 2, 3, 2, 5], [9, 1, 4, 4, 11]));
+
+// Anagrams
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+//my answer
+
+const myAnagram = (s1, s2) => {
+  if (s1.length !== s2.length) {
+    return false;
+  }
+  const sortedS1 = s1.split('').sort().join('');
+  const sortedS2 = s2.split('').sort().join('');
+
+  return sortedS1 === sortedS2;
+}
+
+console.log(myAnagram('anagram', 'nagaram'));
+console.log(myAnagram('bae', 'bba'));
+
+// solution
+const validAnagram = (first, second) => {
+  if (first.length !== second.length) {
+    return false
+  }
+  const lookup = {};
+  for(let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    //if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    //can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(validAnagram('', '')); //true
+validAnagram('aaz', 'zza') //false
+console.log(validAnagram('anagram', 'nagaram')); //true
+validAnagram('rat', 'car') // false
+
