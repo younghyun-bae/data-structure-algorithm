6f77a887fe5245c847c3c1081819e3c8429e876d
Younghyun <yhbae97@gmail.com>
2024-05-27 18:14:43 +0200

    add Multiple pointers example

--- src/3-problem-solving-approach.js
+++ src/3-problem-solving-approach.js
@@ -220,3 +220,47 @@ validAnagram('aaz', 'zza') //false
 console.log(validAnagram('anagram', 'nagaram')); //true
 validAnagram('rat', 'car') // false
 
+// Multiple Pointers
+// Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
+// very efficient for solving problems with minimal space complexity as well
+
+// An Example
+// Write a function called sumZero which accepts a sorted array of intergers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+// Naive solution
+// Time complexity - o(n^2) / Space complexity - O(1)
+
+const sumZero = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+}
+
+console.log(sumZero([-3, -2, -1, 0 ,1, 2, 3])); // [-3, 3]
+sumZero([-2, 0 ,1, 3]); // undefined
+sumZero([1, 2, 3]); // undefined
+
+// Refactor
+// Time complexity - O(n) / Space Complexity - O(1)
+const sumZero2 = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+console.log(sumZero2([-4, -3, -2, -1, 0, 1, 2, 3, 10]));
+console.log(sumZero2([-2, 0 ,1, 3]));
+
