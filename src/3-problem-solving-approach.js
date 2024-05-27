9f1c3a6ee20de6da57af5230998b31ccad374b3e
Younghyun <yhbae97@gmail.com>
2024-05-27 18:53:16 +0200

    add Sliding Window example

--- src/3-problem-solving-approach.js
+++ src/3-problem-solving-approach.js
@@ -284,3 +284,48 @@ console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2])); // 2
 console.log(countUniqueValues([])); // 0
 console.log(countUniqueValues([1, 1, 2, 3, 3, 4, 5, 6, 6, 7]));
 
+// Sliding Window
+/* This pattern involves creating a window which can either be an array or number from one position to another. Depending on a certain condition, the window either increases or closes (and a new window is created). Very useful for keeping track of a subset of data in an array/string etc. */
+
+/* Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n censecutive elements in the array. */
+
+// A naive solution
+// time complexity - O(n^2)
+const maxSubarraySum = (arr, num) => {
+  if (num > arr.length) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i ++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+// Refactor
+// Time complexity - O(n)
+
+const maxSubarraySum2 = (arr, num) => {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+console.log(maxSubarraySum2([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
