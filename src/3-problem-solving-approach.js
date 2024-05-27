f7b4d1d19b7faf1ff9d1953977acdd12ff5c2b05
Younghyun <yhbae97@gmail.com>
2024-05-27 18:34:07 +0200

    add Count Unique Values exmaple

--- src/3-problem-solving-approach.js
+++ src/3-problem-solving-approach.js
@@ -264,3 +264,23 @@ const sumZero2 = (arr) => {
 console.log(sumZero2([-4, -3, -2, -1, 0, 1, 2, 3, 10]));
 console.log(sumZero2([-2, 0 ,1, 3]));
 
+// countUniqueValues
+/* Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted. */
+
+const countUniqueValues = (arr) => {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = i; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j]
+    }
+    console.log(i, j);
+    return i + 1;
+  }
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2])); // 2
+console.log(countUniqueValues([])); // 0
+console.log(countUniqueValues([1, 1, 2, 3, 3, 4, 5, 6, 6, 7]));
+
