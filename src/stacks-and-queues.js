7ff86b0803dad03dc0e9a8da42290a2a3e94c955
Younghyun <yhbae97@gmail.com>
2024-11-24 11:11:04 +0100

    Learn stacks and queues

---
+++ src/stacks-and-queues.js
@@ -0,0 +1,78 @@
+// Stack - last in, first out ex. pop, push for insertion and removal O(1) / queue is opposite
+/* Managing function invocations, undo and redo, routing (the history object) is treated like a stack */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if(!this.first) return null;
+    var temp = this.first;
+    if(this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+// Queue - First in first out ex. it's inefficient using js methods, let's make queue from scratch
+/* Background tasks, Uploading resources, Printing / Task processing */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    var newNode = new Node(val);
+    if(!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue() {
+    if(!this.first) return null;
+    var temp = this.first;
+    if(this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
